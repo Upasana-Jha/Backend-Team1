ec843454ed19c3fe2d859b50e2593dcd00d13b7b
deepikaneeluru <47291186+deepikaneeluru@users.noreply.github.com>
2021-09-12 23:05:57 +0530

    Update api-attendance.js

--- routes/api-attendance.js
+++ routes/api-attendance.js
@@ -1,39 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var {addAttendance,getAttendance,deleteAttendance,getAttendanceByEmployeeId,updateAttendance} = require('../service/attendance-psql.js')
+const { Sequelize } = require('sequelize');
+const Attendance = require('../models/attendance')
 
-
-//api/employee
 router.get('/', async (req, res, next) => {
-  let records = await getAttendance();
-  res.send(records);
+   await Attendance.findAll().then((records)=>{
+    res.send(records);
+   });
+  
 });
-router.get('/', async (req, res, next) => {
-  let records = await getAttendance();
-  res.send(records);
-});
-
 router.get('/:id', async function (req, res) {
-  console.log("id:"+req.params.id);
-  let record = await getAttendanceByEmployeeId(req.params.id);
-  res.send(record);
+  await Attendance.findOne({where:{employeeid:req.params.id}}).then((records)=>{
+    res.send(records);
+   });
 });
 
 router.post('/', async function (req, res) {
-  await addAttendance(req.body);
+  await Attendance.create(req.body);
   res.send({result:"ok", msg:"Attendance added ok"});
 });
 
-router.put('/', async function (req, res) {
-  await updateAttendance(req.body);
-  res.send({result:"ok", msg:"Attendance updated ok"});
+router.delete('/', async function (req, res) {
+  await Attendance.destroy({where:{id:req.body.id}}).then(result=>
+    res.send({result:'success',msg:"Attendance deleted successfully"}),
+    err =>
+    res.send({result:'fail',msg:"Attendancedeletion failed"}))
+ 
 });
 
-
-router.delete('/', async function (req, res) {
-  await deleteAttendance(req.body)
-  res.send({result:"ok", msg:"Attendance deleted ok"}); //response to client
+router.put('/', async function (req, res) {
+  await Attendance.update(req.body,{where :{id:req.body.id}}).then(result =>
+    res.send({result:'success', msg:"Attendance updated successfully"}),
+  err =>
+    res.send({result:'fail', msg:"Attendance updatation failed"})
+  );
+  
 });
 
 
