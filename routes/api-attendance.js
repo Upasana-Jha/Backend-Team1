25e73615326c0888606c47047b826e994d6ef6ea
Ramya Sree <56461863+ramyasree19@users.noreply.github.com>
2021-09-13 08:26:17 +0530

    Update api-attendance.js

--- routes/api-attendance.js
+++ routes/api-attendance.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res, next) => {
   
 });
 router.get('/:id', async function (req, res) {
-  await Attendance.findOne({where:{employeeid:req.params.id}}).then((records)=>{
+  await Attendance.findOne({where:{employeeid:req.params.employeeid}}).then((records)=>{
     res.send(records);
    });
 });
